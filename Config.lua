68b5570fe64edb753a149b30f8b119ffeabb6dd7
Hakan Çolak <61948938+Wizaardd@users.noreply.github.com>
2024-05-15 09:43:40 +0300

    v0.0.5 OLD ESX

--- Config.lua
+++ Config.lua
@@ -2,4 +2,4 @@ Config = {}
 
 Config.MySQL = "oxmysql" -- Suports both mysql-async and oxmysql
 
-Config.Framework = "QB" -- Suports both ESX and QB
+Config.Framework = "QB" -- Suports both ESX, QB and OLD-ESX
